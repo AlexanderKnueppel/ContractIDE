7fa69cb5a4542b3c388a22f6daa434c82fd1a9b1
Alexander Knueppel <a.knueppel@tu-bs.de>
2018-09-22 15:08:26 +0200

    Add methods for model/diagram access

---
+++ plugins/de.tu-bs.ccc.contracting.core/src/de/tu_bs/ccc/contracting/core/util/CoreUtil.java
@@ -0,0 +1,166 @@
+package de.tu_bs.ccc.contracting.core.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.common.util.WrappedException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+import de.tu_bs.ccc.contracting.Verification.Module;
+
+public class CoreUtil {
+
+	public static IProject getCurrentProject() {
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IWorkbenchPage activePage = window.getActivePage();
+
+		IEditorPart activeEditor = activePage.getActiveEditor();
+
+		if (activeEditor != null) {
+			IEditorInput input = activeEditor.getEditorInput();
+
+			IProject project = input.getAdapter(IProject.class);
+			if (project == null) {
+				IResource resource = input.getAdapter(IResource.class);
+				if (resource != null) {
+					project = resource.getProject();
+				}
+			}
+			return project;
+		}
+
+		return null;
+	}
+
+	public static Collection<Module> getRootModules(IProject p) {
+		final List<IFile> files = getModelFiles(p);
+		final List<Module> moduleList = new ArrayList<Module>();
+		final ResourceSet rSet = new ResourceSetImpl();
+		for (final IFile file : files) {
+			final Module module = getRootModuleFromFile(file, rSet);
+			if (module != null) {
+				moduleList.add(module);
+			}
+		}
+		return moduleList;
+	}
+
+	private static List<IFile> getModelFiles(IContainer folder) {
+		final List<IFile> ret = new ArrayList<IFile>();
+		try {
+			final IResource[] members = folder.members();
+			for (final IResource resource : members) {
+				if (resource instanceof IContainer) {
+					ret.addAll(getModelFiles((IContainer) resource));
+				} else if (resource instanceof IFile) {
+					final IFile file = (IFile) resource;
+					if (file.getName().endsWith(".model")) {
+						ret.add(file);
+					}
+				}
+			}
+		} catch (final CoreException e) {
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
+	private static Module getRootModuleFromFile(IFile file, ResourceSet resourceSet) {
+		// Get the URI of the model file.
+		final URI resourceURI = getFileURI(file, resourceSet);
+		// Demand load the resource for this file.
+		Resource resource;
+		try {
+			resource = resourceSet.getResource(resourceURI, true);
+			if (resource != null) {
+				// does resource contain a diagram as root object?
+				final EObject root = resource.getContents().get(0);
+				if (root instanceof Module) {
+					return (Module) root;
+				}
+			}
+		} catch (final WrappedException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Collection<Diagram> getDiagrams(IProject p) {
+		final List<IFile> files = getDiagramFiles(p);
+		final List<Diagram> diagramList = new ArrayList<Diagram>();
+		final ResourceSet rSet = new ResourceSetImpl();
+		for (final IFile file : files) {
+			final Diagram diagram = getDiagramFromFile(file, rSet);
+			if (diagram != null) {
+				diagramList.add(diagram);
+			}
+		}
+		return diagramList;
+	}
+
+	private static List<IFile> getDiagramFiles(IContainer folder) {
+		final List<IFile> ret = new ArrayList<IFile>();
+		try {
+			final IResource[] members = folder.members();
+			for (final IResource resource : members) {
+				if (resource instanceof IContainer) {
+					ret.addAll(getDiagramFiles((IContainer) resource));
+				} else if (resource instanceof IFile) {
+					final IFile file = (IFile) resource;
+					if (file.getName().endsWith(".diagram")) {
+						ret.add(file);
+					}
+				}
+			}
+		} catch (final CoreException e) {
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
+	private static Diagram getDiagramFromFile(IFile file, ResourceSet resourceSet) {
+		// Get the URI of the model file.
+		final URI resourceURI = getFileURI(file, resourceSet);
+		// Demand load the resource for this file.
+		Resource resource;
+		try {
+			resource = resourceSet.getResource(resourceURI, true);
+			if (resource != null) {
+				// does resource contain a diagram as root object?
+				final EList<EObject> contents = resource.getContents();
+				for (final EObject object : contents) {
+					if (object instanceof Diagram) {
+						return (Diagram) object;
+					}
+				}
+			}
+		} catch (final WrappedException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static URI getFileURI(IFile file, ResourceSet resourceSet) {
+		final String pathName = file.getFullPath().toString();
+		URI resourceURI = URI.createFileURI(pathName);
+		resourceURI = resourceSet.getURIConverter().normalize(resourceURI);
+		return resourceURI;
+	}
+}
