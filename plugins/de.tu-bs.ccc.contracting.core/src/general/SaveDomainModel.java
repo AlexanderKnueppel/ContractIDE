16f0300a1e676ccd0e6374fcc0d1d713a05c18ee
neapel <h.fenske@tu-braunschweig.de>
2018-09-27 07:48:52 +0200

    delete verify popup when adding new features, add 16x16x size of icon

--- plugins/de.tu-bs.ccc.contracting.core/src/general/SaveDomainModel.java
+++ plugins/de.tu-bs.ccc.contracting.core/src/general/SaveDomainModel.java
@@ -3,8 +3,6 @@ package general;
 import java.io.IOException;
 import java.util.HashMap;
 
-import javax.swing.JOptionPane;
-
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -15,13 +13,10 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
 
-import de.tu_bs.ccc.contracting.Verification.Component;
-import de.tu_bs.ccc.contracting.Verification.Contract;
 
 public class SaveDomainModel {
 	public static void saveToModelFile(final EObject obj, final Diagram d) throws CoreException, IOException {
 		URI uri = d.eResource().getURI();
-		JOptionPane.showConfirmDialog(null, uri.toString());
 		uri = uri.trimFragment();
 		uri = uri.trimFileExtension();
 		uri = uri.appendFileExtension("model");
