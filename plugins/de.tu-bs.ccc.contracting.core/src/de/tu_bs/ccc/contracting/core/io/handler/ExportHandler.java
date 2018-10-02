0660cbb8bf7292c02d58100f35b8fb72cc352830
Alexander Knueppel <a.knueppel@tu-bs.de>
2018-10-02 08:50:14 +0200

    Move and implement export handler

---
+++ plugins/de.tu-bs.ccc.contracting.core/src/de/tu_bs/ccc/contracting/core/io/handler/ExportHandler.java
@@ -0,0 +1,41 @@
+package de.tu_bs.ccc.contracting.core.io.handler;
+
+import java.io.File;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+import de.tu_bs.ccc.contracting.Verification.Module;
+import de.tu_bs.ccc.contracting.core.io.ccc.CCCExporter;
+import de.tu_bs.ccc.contracting.core.util.CoreUtil;
+
+public class ExportHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+	    if (window != null)
+	    {
+	        IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
+	        Object firstElement = selection.getFirstElement();
+	        if (firstElement instanceof IAdaptable && firstElement instanceof IFile)
+	        {
+	            IFile file = (IFile)((IAdaptable)firstElement).getAdapter(IFile.class);
+	    		Module m = CoreUtil.getRootModuleFromFile(file, new ResourceSetImpl());
+	           //Module m = CoreUtil.getRootModuleFromFile(file, new ResourceSetImpl());
+	    		if(m != null)
+	    			new CCCExporter(m).writeToFile(new File("./export.xml"));
+	        }
+	    }
+		return null;
+	}
+
+}
