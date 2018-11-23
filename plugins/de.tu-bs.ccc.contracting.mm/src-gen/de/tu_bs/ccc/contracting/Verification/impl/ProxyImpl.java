856656748638872286bf154ae1c0dc32b54e45b6
Alexander Knueppel <a.knueppel@tu-bs.de>
2018-11-23 13:07:49 +0100

    Generated for meta model

---
+++ plugins/de.tu-bs.ccc.contracting.mm/src-gen/de/tu_bs/ccc/contracting/Verification/impl/ProxyImpl.java
@@ -0,0 +1,163 @@
+/**
+ */
+package de.tu_bs.ccc.contracting.Verification.impl;
+
+import de.tu_bs.ccc.contracting.Verification.MmPackage;
+import de.tu_bs.ccc.contracting.Verification.Proxy;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Proxy</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * </p>
+ * <ul>
+ *   <li>{@link de.tu_bs.ccc.contracting.Verification.impl.ProxyImpl#getCarrier <em>Carrier</em>}</li>
+ * </ul>
+ *
+ * @generated
+ */
+public class ProxyImpl extends ModuleTypeImpl implements Proxy {
+	/**
+	 * The default value of the '{@link #getCarrier() <em>Carrier</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getCarrier()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String CARRIER_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getCarrier() <em>Carrier</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getCarrier()
+	 * @generated
+	 * @ordered
+	 */
+	protected String carrier = CARRIER_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ProxyImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return MmPackage.Literals.PROXY;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getCarrier() {
+		return carrier;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setCarrier(String newCarrier) {
+		String oldCarrier = carrier;
+		carrier = newCarrier;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, MmPackage.PROXY__CARRIER, oldCarrier, carrier));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+		case MmPackage.PROXY__CARRIER:
+			return getCarrier();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+		case MmPackage.PROXY__CARRIER:
+			setCarrier((String) newValue);
+			return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+		case MmPackage.PROXY__CARRIER:
+			setCarrier(CARRIER_EDEFAULT);
+			return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+		case MmPackage.PROXY__CARRIER:
+			return CARRIER_EDEFAULT == null ? carrier != null : !CARRIER_EDEFAULT.equals(carrier);
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy())
+			return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (carrier: ");
+		result.append(carrier);
+		result.append(')');
+		return result.toString();
+	}
+
+} //ProxyImpl
