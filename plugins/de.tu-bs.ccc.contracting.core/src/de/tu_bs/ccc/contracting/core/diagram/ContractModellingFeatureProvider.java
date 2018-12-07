2865cbab45feb020e6f1f8a45d13dd9ebf6c88ad
Sabrina Hugo <s.hugo@tu-bs.de>
2018-12-07 11:25:22 +0100

    Revert "Solved Issue 30"
    This reverts commit 1e59c65684e10733b67f91caaf72028c5fc19302.

--- plugins/de.tu-bs.ccc.contracting.core/src/de/tu_bs/ccc/contracting/core/diagram/ContractModellingFeatureProvider.java
+++ plugins/de.tu-bs.ccc.contracting.core/src/de/tu_bs/ccc/contracting/core/diagram/ContractModellingFeatureProvider.java
@@ -141,6 +141,7 @@ public class ContractModellingFeatureProvider extends DefaultFeatureProvider {
 		if (bo instanceof Ports || bo instanceof Module || bo instanceof Contract || bo instanceof System) {
 			return new LayoutFeature(this);
 		}
+
 		return super.getLayoutFeature(context);
 	}
 
