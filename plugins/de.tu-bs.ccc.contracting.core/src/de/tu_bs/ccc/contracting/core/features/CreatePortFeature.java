bb6dce6260b292f6957225b6dad1009cc054d810
Markus Gamperle <M.Gamperle@gmail.com>
2019-02-18 08:59:23 +0100

    added preventing adding port to already used component

--- plugins/de.tu-bs.ccc.contracting.core/src/de/tu_bs/ccc/contracting/core/features/CreatePortFeature.java
+++ plugins/de.tu-bs.ccc.contracting.core/src/de/tu_bs/ccc/contracting/core/features/CreatePortFeature.java
@@ -1,6 +1,5 @@
 package de.tu_bs.ccc.contracting.core.features;
 
-
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.graphiti.features.ICreateFeature;
@@ -11,13 +10,16 @@ import org.eclipse.graphiti.features.impl.AbstractCreateFeature;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 import de.tu_bs.ccc.contracting.Verification.Module;
 import de.tu_bs.ccc.contracting.Verification.PortType;
 import de.tu_bs.ccc.contracting.Verification.Ports;
 import de.tu_bs.ccc.contracting.Verification.ProviderType;
+import de.tu_bs.ccc.contracting.core.mapping.ImportMapping;
 import de.tu_bs.ccc.contracting.Verification.MmFactory;
-
 
 public class CreatePortFeature extends AbstractCreateFeature implements ICreateFeature {
 	public static final PortType pt = PortType.STRING;
@@ -32,11 +34,11 @@ public class CreatePortFeature extends AbstractCreateFeature implements ICreateF
 		if (!(context.getTargetContainer() instanceof Diagram)) {
 			PictogramElement pict = context.getTargetContainer();
 			if (!(pict instanceof ContainerShape)) {
-				
+
 				return false;
 			}
-			
 			EList<EObject> businessObjects = pict.getLink().getBusinessObjects();
+			
 			return businessObjects.size() == 1 && businessObjects.get(0) instanceof Module;
 		} else {
 			return false;
@@ -45,21 +47,33 @@ public class CreatePortFeature extends AbstractCreateFeature implements ICreateF
 
 	@Override
 	public Object[] create(ICreateContext context) {
-		Ports addedObject = MmFactory.eINSTANCE.createPorts();
-    	
-    	addedObject.setType(pt);
 		PictogramElement pict = context.getTargetContainer();
 		EList<EObject> businessObjects = pict.getLink().getBusinessObjects();
+		if (businessObjects.get(0) instanceof Module) {
+			Module m = (Module) businessObjects.get(0);
+			if (ImportMapping.getMappingEntry(m).size() != 0) {
+				Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+				MessageDialog dialog = new MessageDialog(shell, "Componetent already instanced", null,
+						"You can not add ports to an component with instances!", MessageDialog.INFORMATION,
+						new String[] { "OK" }, 0);
+				dialog.open();
+				return null;
+			}
+
+		}
+		Ports addedObject = MmFactory.eINSTANCE.createPorts();
+
+		addedObject.setType(pt);
 		Module expandWithPort = ((Module) businessObjects.get(0));
-		String classname = "port"+expandWithPort.getPorts().size();
+		String classname = "port" + expandWithPort.getPorts().size();
 		addedObject.setName(classname);
-		
+
 		addedObject.setLabel("");
 		addedObject.setFunction("");
 		addedObject.setFilter("");
 		addedObject.setProviderType(ProviderType.NATIVE);
 		addedObject.setMaxClients(-1);
-		
+
 		expandWithPort.getPorts().add(addedObject);
 		AddContext contextAdd = new AddContext(context, addedObject);
 		contextAdd.setTargetContainer(context.getTargetContainer());
