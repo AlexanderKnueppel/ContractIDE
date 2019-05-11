d0c73edc060c948f4e38783b062f7b08644158cb
Markus Gamperle <M.Gamperle@gmail.com>
2019-05-11 09:40:09 +0200

    updated Converter

--- plugins/de.tu-bs.ccc.contracting.core/src/solver/Z3Converter.java
+++ plugins/de.tu-bs.ccc.contracting.core/src/solver/Z3Converter.java
@@ -1,23 +1,35 @@
 package solver;
 
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+
 import com.microsoft.z3.Context;
 import com.microsoft.z3.FuncDecl;
 import com.microsoft.z3.Solver;
 import com.microsoft.z3.Symbol;
 
+import de.tu_bs.ccc.contracting.Verification.Assumption;
 import de.tu_bs.ccc.contracting.Verification.Compound;
+import de.tu_bs.ccc.contracting.Verification.Contract;
+import de.tu_bs.ccc.contracting.Verification.ContractProperty;
+import de.tu_bs.ccc.contracting.Verification.Guarantee;
+import de.tu_bs.ccc.contracting.core.grammar.GrammarSolver;
 
 public class Z3Converter {
+	private static GrammarSolver gs = new GrammarSolver();
+	public static Z3Converter z= new Z3Converter();
+
 	public static String[] getNames(Compound m) {
-		String[] names = new String[m.getConsistsOf().size()+1];
-		names[0]= m.getName();
-		for (int i = 1; i < m.getConsistsOf().size()+1; i++) {
-			names[i]= m.getConsistsOf().get(i).getName();
+		String[] names = new String[m.getConsistsOf().size() + 1];
+		names[0] = m.getName();
+		for (int i = 1; i < m.getConsistsOf().size() + 1; i++) {
+			names[i] = m.getConsistsOf().get(i).getName();
 		}
 		return names;
-		
-		
+
 	}
+
 	public static void addAssetion(String assertion, Context ctx, Solver s, Symbol[] declNames, FuncDecl[] decl) {
 
 		s.add(ctx.parseSMTLIB2String("(assert " + assertion + ")", null, null, declNames, decl));
@@ -45,5 +57,34 @@ public class Z3Converter {
 		}
 		return func;
 	}
+
+	public static boolean ContractCheck(Contract c,Context ctx,Solver s,Symbol[] symbols,FuncDecl[] func) {
+		s.add(ctx.parseSMTLIB2String(
+				"(define-fun ValidContract () Bool (=> "  + getProperties(c.getAssumption())  + getProperties(c.getGuarantee())+ "))(assert (not ValidContract))",
+				null, null, symbols, func));
+		return false;
+		
+	}
+
+	private static String getProperties(EList<? extends ContractProperty> eList) {
+		String z3String = new String();
+		for (ContractProperty contractProperty : eList) {
+			z3String += "( and";
+		}
+		if (eList.size()>0) {
+			z3String += gs.parseString(eList.get(0).getProperty())+" ";
+		}
+		for (int i = 1; i < eList.size(); i++) {
+			
+		
+			z3String += gs.parseString(eList.get(i).getProperty()) + ")";
+		}
+		
+
+		return z3String;
+	}
+	
+
+
 
 }
