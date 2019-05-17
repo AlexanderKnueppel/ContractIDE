5db8dcb68c0d3a21eb1b9ba52fee0f0683285d39
Markus Gamperle <M.Gamperle@gmail.com>
2019-05-17 11:40:47 +0200

    added a class with colors for coloring the diagram objects and added used strings to a table

--- plugins/de.tu-bs.ccc.contracting.core/src/de/tu_bs/ccc/contracting/core/localization/StringTable.java
+++ plugins/de.tu-bs.ccc.contracting.core/src/de/tu_bs/ccc/contracting/core/localization/StringTable.java
@@ -6,4 +6,23 @@ public class StringTable {
 	public static final String PROJECT_SRC = "src";
 	public static final String PROJECT_SRC_SYSTEM = "src/system";
 	public static final String PROJECT_SRC_MODEL = "src/repository";
+
+	// Feature Strings (on mouse over)
+	public static final String CREATE_ABSTRACT = "cide";
+	public static final String CREATE_ABSTRACT_SHORT = "cide";
+	public static final String CREATE_COMPONENT = "cide";
+	public static final String CREATE_COMPONENT_SHORT = "cide";
+	public static final String CREATE_SYSTEM = "cide";
+	public static final String CREATE_SYSTEM_SHORT = "cide";
+	public static final String CREATE_COMPOUND = "cide";
+	public static final String CREATE_COMPOUND_SHORT = "cide";
+	public static final String CREATE_CONTRACT_CONNECTION = "cide";
+	public static final String CREATE_CONTRACT_CONNECTION_SHORT = "cide";
+	public static final String CREATE_CONTRACT = "cide";
+	public static final String CREATE_CONTRACT_SHORT = "cide";
+	public static final String CREATE_ASSUMPTION= "cide";
+	public static final String CREATE_PROPERTY_SHORT = "cide";
+	public static final String CREATE_GUARANTEE = "cide";
+	
+
 }
